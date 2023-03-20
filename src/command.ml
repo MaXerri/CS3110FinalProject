d0bb43c6c9544049e47af49fbf1cbfced995e322
Mario <max3@cornell.edu>
2023-03-19 20:40:44 -0400

    Created command.ml and command.mli files
    - Made the command.mli file and started to implement the .ml file
    - This file deals with the command parsing

---
+++ src/command.ml
@@ -0,0 +1,34 @@
+type object_phrase = string list
+
+type command =
+  | Clear of object_phrase
+  | Quit
+
+exception Empty
+exception Malformed
+
+let check_empty str =
+  List.filter (fun x -> x <> "") (String.split_on_char ' ' str) = []
+
+let check_malformed str_list =
+  match str_list with
+  | [] -> raise Empty
+  | h :: t ->
+      if h <> "quit" && h <> "clear" then true
+      else if h = "quit" && t <> [] then true
+      else if h = "clear" && t = [] then true
+      else false
+
+let parse str =
+  if check_empty str then raise Empty
+  else if
+    check_malformed
+      (List.filter (fun x -> x <> "") (String.split_on_char ' ' str))
+  then raise Malformed
+  else
+    match List.filter (fun x -> x <> "") (String.split_on_char ' ' str) with
+    | [] -> raise Empty
+    | h :: t ->
+        if h = "clear" then
+          if List.length t = 2 then Clear t else raise Malformed
+        else Quit
---
+++ src/command.mli
@@ -0,0 +1,25 @@
+type object_phrase = string list
+(** The type [object_phrase] represents the object phrase that can be part of a
+    player command. Each element of the list represents a word of the object
+    phrase, where a "word" is defined as a consecutive sequence of non-space
+    characters. Thus, no element of the list should contain any leading,
+    internal, or trailing spaces. The list is in the same order as the words in
+    the original player command. *)
+
+(** The type [command] represents a player command that is decomposed into a
+    verb and possibly an object phrase. Invariant: the [object_phrase] carried
+    by [Clear] must not be empty. *)
+type command =
+  | Clear of object_phrase
+  | Quit
+
+exception Empty
+(** Raised when an empty command is parsed. *)
+
+exception Malformed
+(** Raised when a malformed command is parsed. *)
+
+val parse : string -> command
+(** [parse str] parses a player's input into a [command], as follows. The first
+    word (i.e., consecutive sequence of non-space characters) of [str] becomes
+    the verb. The rest of the words, if any, become the object phrase.*)
