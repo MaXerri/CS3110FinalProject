57c7cf5abb41630bd6e109d32e16548680f03752
Mario <max3@cornell.edu>
2023-03-20 00:00:14 -0400

    Made main.ml file in the bin folder
    - This file starts a minesweeper game given a boardsize input
    - Not implemented fully
    - having trouble accessing the other files

---
+++ bin/main.ml
@@ -0,0 +1,40 @@
+open Minesweeper
+
+(** [valid f] checks if the board size is a valid boardsize*)
+let valid_size f =
+  let i = String.split_on_char ' ' f in
+  if List.length i <> 2 then false
+  else if
+    (match int_of_string_opt (List.nth i 0) with
+    | Some num -> num
+    | None -> -1)
+    < 0
+    || (match int_of_string_opt (List.nth i 1) with
+       | Some num -> num
+       | None -> -1)
+       < 0
+  then false
+  else true
+
+(** [play_game f] starts a minesweeper game of size f. *)
+let play_game f =
+  let i = String.split_on_char ' ' f in
+  if valid_size f = true then
+    Board.generate (*doesn't work need to fix this *)
+      (int_of_string_opt (List.nth i 0))
+      (int_of_string_opt (List.nth i 1))
+  else print_string "Invalid Input. Try again."
+
+(** [main ()] prompts for the game to play, then starts it. *)
+let main () =
+  print_string "\n\nWelcome to the 3110 Text Adventure Game engine.\n";
+  print_endline
+    "Please enter the size of the game you want to load. Separate the desired \
+     row and column size by 1 space";
+  print_string "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | file_name -> play_game file_name
+
+(* Execute the game engine. *)
+let () = main ()
