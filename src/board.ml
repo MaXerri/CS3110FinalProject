9b4456a0d46eb7f0619e3f70444633d6b4d85233
Lucas Silva <las495@cornell.edu>
2023-03-20 21:30:58 -0400

    Implemented Board.clear_position
    -Implemented the function according to its specifications
    -Added two helper functions mod_indof_list and mod_indof_grid

--- src/board.ml
+++ src/board.ml
@@ -7,8 +7,22 @@ type board = {
 
 (*** Functions ****************************************************************)
 
-let clear_position (brd : board) (tup : int * int) : board =
-  raise (Failure "Unimplemented: Board.clear_position")
+(**[mod_indof_list foi i lst] returns [lst] with the [i]th element of [lst]
+   modified by [foi]*)
+let rec mod_indof_list foi (ind : int) lst =
+  match lst with
+  | [] -> raise (Failure "Index out of bounds")
+  | h :: t ->
+      if ind = 0 then foi h :: t else h :: mod_indof_list foi (ind - 1) t
+
+(**[mod_indof_grid foi (m,n) grd] returns [grd] with the [n]th element of the
+   [m]th element of [grd] modified by [foi]*)
+let mod_indof_grid foi (ind : int * int) (grd : 'a list list) =
+  match ind with
+  | m, n -> mod_indof_list (mod_indof_list foi n) m grd
+
+let clear_position brd (tup : int * int) =
+  { brd with grid = mod_indof_grid Cell.clear tup brd.grid }
 
 let to_string_list = raise (Failure "Unimplemented: Board.to_string_list")
 let display = raise (Failure "Unimplemented: Board.display")
