809132f897fc44779f6c724017f184598e0ded65
Lucas Silva <las495@cornell.edu>
2023-03-20 21:56:45 -0400

    Implemented rudimentary version of Board.generate
    -Implemented function such that it returns a board of empty cells
    -Implemented helper functions generate_grid and generate_list

--- src/board.ml
+++ src/board.ml
@@ -5,7 +5,7 @@ type board = {
 }
 (** The abstract type of values representing a board and game state. *)
 
-(*** Functions ****************************************************************)
+(*** Helper Functions *********************************************************)
 
 (**[mod_indof_list foi i lst] returns [lst] with the [i]th element of [lst]
    modified by [foi]*)
@@ -21,10 +21,31 @@ let mod_indof_grid foi (ind : int * int) (grd : 'a list list) =
   match ind with
   | m, n -> mod_indof_list (mod_indof_list foi n) m grd
 
+(***)
+let rec generate_list controller (l : int) =
+  if l = 0 then [] else controller true :: generate_list controller (l - 1)
+
+let generate_grid controller (m : int) (n : int) =
+  generate_list (fun a -> if a then generate_list controller n else []) m
+
+(*** Functions ****************************************************************)
+
 let clear_position brd (tup : int * int) =
   { brd with grid = mod_indof_grid Cell.clear tup brd.grid }
 
 let to_string_list = raise (Failure "Unimplemented: Board.to_string_list")
 let display = raise (Failure "Unimplemented: Board.display")
-let generate = raise (Failure "Unimplemented: Board.generate")
+
+let generate m n =
+  {
+    grid =
+      generate_grid
+        (fun a ->
+          if a then Cell.generate 0
+          else raise (Failure "Poorly defined controller"))
+        m n;
+    m;
+    n;
+  }
+
 let dimensions brd = (brd.m, brd.n)
