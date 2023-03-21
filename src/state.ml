96362b752f89d32c572260452e16dd0ab598d169
Mario <max3@cornell.edu>
2023-03-20 22:05:29 -0400

    Fixed valid_size function
    - fixed the implementation so it detects if an imput is too large

--- src/state.ml
+++ src/state.ml
@@ -11,27 +11,45 @@ type result =
   | Legal of t
   | Illegal
 
-let valid_size f =
+(*chekcs that both numbers are >0 and chekcs the first # < m and the 2nd number
+  < n. Also checks evaluates every input that isnt 2 numbers separated by a
+  space to a number outside of the acceptable range so it is detected as a
+  incorrect input and no weird errors are thrown *)
+let valid_size f st =
   let i = String.split_on_char ' ' f in
   if List.length i <> 2 then false
   else if
-    (match int_of_string_opt (List.nth i 0) with
-    | Some num -> num
-    | None -> -1)
-    < 0
-    || (match int_of_string_opt (List.nth i 1) with
+    (let x =
+       match int_of_string_opt (List.nth i 0) with
        | Some num -> num
-       | None -> -1)
-       < 0
+       | None -> -1
+     in
+     x < 0
+     || x
+        >
+        match Board.dimensions st.current_board with
+        | a, _ -> a)
+    ||
+    let y =
+      match int_of_string_opt (List.nth i 1) with
+      | Some num -> num
+      | None -> -1
+    in
+    y < 0
+    || y
+       >
+       match Board.dimensions st.current_board with
+       | _, a -> a
   then false
   else true
 
 let clear input st : result =
   let i = String.split_on_char ' ' input in
-  if valid_size input = false then Illegal
+  if valid_size input st = false then Illegal
   else
     Legal
       {
         current_board = raise (Failure "Unimplemented: State.clear");
+        (*Call Board.clear when implemented*)
         game_over = false (*fix this eventually*);
       }
