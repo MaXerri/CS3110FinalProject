3cf9659f42e89667bd3f2b951f08cf2a347def94
Mario <max3@cornell.edu>
2023-03-21 14:05:19 -0400

    Removed diplay function from board.mli
    - This function is not needed becasue printed is done in main

--- src/board.mli
+++ src/board.mli
@@ -19,9 +19,6 @@ val to_string_list : board -> string list
 (** [to_string_list x] is a list of strings which when printed represent the
     board [x]. *)
 
-val display : board -> _
-(** [display x] prints a representation of the board [x] to the command line. *)
-
 val generate : int -> int -> board
 (** [generate m n] is a board with dimensions [m] columns and [n] rows with no
     tiles revealed. *)
