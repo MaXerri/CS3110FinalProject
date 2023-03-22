441b8c1e758342077b869df13da2352813e13593
sfp58 <sfp58@cornell.edu>
2023-03-22 19:06:48 -0400

    Added skeleton structure of test suite

--- test/main.ml
+++ test/main.ml
@@ -5,5 +5,16 @@ open Cell
 open Command
 open State
 
+let board_test = []
 
+let cell_test = []
 
+let command_test = []
+
+let state_test = []
+
+let suite = 
+  "Test Suite for Board Cell Command and State"
+  >::: List.flatten [board_test; cell_test; command_test; state_test]
+
+let _ = run_test_tt_main suite
