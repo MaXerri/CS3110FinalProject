2339e3cc66d569be13f1d8a557465e10694c09e1
sfp58 <sfp58@cornell.edu>
2023-03-22 21:27:28 -0400

    Fixed typos in main.ml strings to be consistent

--- test/main.ml
+++ test/main.ml
@@ -18,6 +18,7 @@ let v5Cell = generate (5);;
 let v6Cell = generate (6);;
 let v7Cell = generate (7);;
 let v8Cell = generate (8);;
+let badV9Cell = generate (9);;
 let badCell = generate (-2);;
 
 
@@ -40,14 +41,15 @@ let cell_test = [
   generate_test "Generate Mine cell" (-1) mineCell;
   generate_test "Generate Empty cell" (0) emptyCell;
   generate_test "Generate value 1 cell" (1) v1Cell;
-  generate_test "Generate value 1 cell" (2) v2Cell;
-  generate_test "Generate value 1 cell" (3) v3Cell;
-  generate_test "Generate value 1 cell" (4) v4Cell;
-  generate_test "Generate value 1 cell" (5) v5Cell;
-  generate_test "Generate value 1 cell" (6) v6Cell;
-  generate_test "Generate value 1 cell" (7) v7Cell;
-  generate_test "Generate value 1 cell" (8) v8Cell;
-  generate_test "Generate value 1 cell" (-2) badCell
+  generate_test "Generate value 2 cell" (2) v2Cell;
+  generate_test "Generate value 3 cell" (3) v3Cell;
+  generate_test "Generate value 4 cell" (4) v4Cell;
+  generate_test "Generate value 5 cell" (5) v5Cell;
+  generate_test "Generate value 6 cell" (6) v6Cell;
+  generate_test "Generate value 7 cell" (7) v7Cell;
+  generate_test "Generate value 8 cell" (8) v8Cell;
+  generate_test "Generate value 9 cell" (9) badV9Cell;
+  generate_test "Generate value -2 cell" (-2) badCell
 
 ]
 
