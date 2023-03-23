fa64fcfe0ba9773c8d03630c1cacc441a426cee1
sfp58 <sfp58@cornell.edu>
2023-03-22 21:18:01 -0400

    Added temporary cell values for testing Added a printer function for cells Added rudimentary testing of Cell.generate

--- test/main.ml
+++ test/main.ml
@@ -7,19 +7,32 @@ open State
 
 (*let cmp_cells (cell1 : cell) (cell2 : cell) = true*)
 
+(*temporary*)
+let mineCell = generate (-1);;
+let emptyCell = generate (0);;
+let v1Cell = generate (1);;
+
+
+
+(*Cell pretty print*)
+let pp_cellValue (c : cell) : string = Cell.to_int c |> string_of_int
 
 (*Cell generate helper function*)
 let generate_test (name : string) (input : int) (expected_output: cell) = 
   name >:: fun _ ->
-    assert_equal expected_output (generate input)
+    assert_equal expected_output (generate input) ~printer: pp_cellValue
 
 
 
 
-
+(*Lists of tests*)
 let board_test = []
 
-let cell_test = []
+let cell_test = [
+  generate_test "Generate Mine cell" (-1) mineCell;
+  generate_test "Generate Empty cell" (0) emptyCell;
+  generate_test "Generate value 1 cell" (1) v1Cell
+]
 
 let command_test = []
 
